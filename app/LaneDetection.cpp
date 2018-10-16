d6a33896f114f111dd6e2904c8e7f2c8307856b1
Akash Guha <akaguha@terpmail.umd.edu>
2018-10-15 21:08:45 -0400

    Refactor: Update LaneDetection.cpp and LaneDetection.hpp

--- app/LaneDetection.cpp
+++ app/LaneDetection.cpp
@@ -1,14 +1,40 @@
+/************************************************************************
+ MIT License
+
+ Copyright (c) 2018 Rohith Jayarajan, Akash Guha
+
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ *************************************************************************/
 /**
- *  Copyright [2018] Akash Guha
+ *  Copyright 2018 Akash Guha, rohithjayarajan
  *  @file    LaneDetection.cpp
- *  @author  Akash Guha
+ *  @author  Akash Guha, rohithjayarajan
+ *  @date 10/13/2018
+ *  @version 1.1
  *
  *  @brief Lane Detection Class file
  *
  *  @section DESCRIPTION
  *
  *  Class for lane detection. This
- *  includes definations all functionalities to
+ *  includes definitions all functionalities to
  *  implement a lane detection system and to predict
  *  steering information for the vehicle.
  *
@@ -30,7 +56,7 @@ LaneDetection::LaneDetection() {
  *   @param nothing
  *   @return nothing
  */
-virtual LaneDetection::~LaneDetection() {
+LaneDetection::~LaneDetection() {
 }
 /**
  *   @brief Function to undistort the input image
@@ -38,89 +64,59 @@ virtual LaneDetection::~LaneDetection() {
  *   @param input image of type cv::Mat
  *   @return undistorted input image of type cv::Mat
  */
-cv::Mat LaneDetection::unDistort(cv::Mat inputImg) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to denoise undistorted input image
- *
- *   @param undistorted image of type cv::Mat
- *   @return denoised image of type cv::Mat
- */
-cv::Mat LaneDetection::deNoise(cv::Mat undistortedImg) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to denoise image
- *
- *   @param denoised image of type cv::Mat
- *   @return masked image of type cv::Mat
- */
-cv::Mat LaneDetection::ROI(cv::Mat denoisedImg) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to get a binary image based on the color thresholding
- *
- *   @param masked image of type cv::Mat
- *   @param thresholding color space of type string
- *   @return binary image of type cv::Mat
- */
-cv::Mat LaneDetection::colorThreshold(cv::Mat maskedImg,
-                                      std::string colorSpace) {
-  return cv::Mat;
-}
 /**
  *   @brief Function to generate lane pixel histogram
  *
  *   @param projective transform of binary image of type cv::Mat
- *   @return histogram containing lane pixels of type cv::Mat
+ *   @param histogram containing lane pixels of type cv::Mat
+ *   @return nothing
  */
-cv::Mat LaneDetection::generateHist(cv::Mat birdView) {
-  return cv::Mat;
+void LaneDetection::generateHist(cv::Mat& src, cv::Mat& hist) {
 }
 /**
  *   @brief Function to extract left or right lane
  *
  *   @param histogram containing lane pixels of type cv::Mat
+ *   @param pixel locations of left or right lane, type std::vector<cv::Point_<int>>
  *   @param lane to be extracted - left or right of type string
- *   @return pixel locations of left or right lane, type std::vector<cv::Point_<int>>
+ *   @return nothing
  */
-std::vector<cv::Point_<int>> LaneDetection::extractLane(cv::Mat hist,
-                                                        std::string lane) {
-  return std::vector<cv::Point_<int>>;
+void LaneDetection::extractLane(cv::Mat& hist,
+                                std::vector<cv::Point_<int>>& dstLane,
+                                std::string& laneType) {
 }
 /**
  *   @brief Function to fit a polynomial on the received lane pixel data
  *
  *   @param left or right lane pixel locations, type std::vector<cv::Point_<int>>
- *   @return pixel locations fitted to a particular polynomial, type std::vector<cv::Point_<int>>
+ *   @param pixel locations fitted to a particular polynomial, type std::vector<cv::Point_<int>>
+ *   @return nothing
  */
-std::vector<cv::Point_<int>> LaneDetection::fitPoly(
-    std::vector<cv::Point_<int>> laneLR) {
-  return std::vector<cv::Point_<int>>;
+void LaneDetection::fitPoly(std::vector<cv::Point_<int>> laneLR,
+                            std::vector<cv::Point_<int>> dstPixelPoly) {
 }
 /**
  *   @brief Function to extract central line
  *
  *   @param left lane pixel locations, type std::vector<cv::Point_<int>>
  *   @param right lane pixel locations, type std::vector<cv::Point_<int>>
- *   @return central line pixel locations, type std::vector<cv::Point_<int>>
+ *   @param central line pixel locations, type std::vector<cv::Point_<int>>
+ *   @return nothing
  */
-std::vector<cv::Point_<int>> LaneDetection::extractCentralLine(
+void LaneDetection::extractCentralLine(
     std::vector<cv::Point_<int>> leftLane,
-    std::vector<cv::Point_<int>> rightLane) {
-  return std::vector<cv::Point_<int>>;
+                        std::vector<cv::Point_<int>> rightLane,
+    std::vector<cv::Point_<int>> centralLine) {
 }
 /**
  *   @brief Function to compute slope of the central line
  *
  *   @param central line pixel location, type std::vector<cv::Point_<int>>
- *   @return string, indicating left or right turn
+ *   @param string, indicating left or right turn
+ *   @return nothing
  */
-std::string LaneDetection::computeGradient(
-    std::vector<cv::Point_<int>> centralLine) {
-  return std::string;
+void LaneDetection::computeGradient(std::vector<cv::Point_<int>> centralLine,
+                                    std::string steerDirection) {
 }
 /**
  *   @brief Function to compute radius of curvature of the lane
@@ -131,208 +127,4 @@ std::string LaneDetection::computeGradient(
 double LaneDetection::computeRadiusOfCurve(
     std::vector<cv::Point_<int>> centralLine) {
   return 0.0;
-}
-/**
- *   @brief Function to set size of the input image
- *
- *   @param input image size of type cv::Size_<int>
- *   @return nothing
- */
-void LaneDetection::setImgSize(cv::Size_<int>& imgSize) {
-}
-/**
- *   @brief Function to set input image
- *
- *   @param input image of type cv::Mat
- *   @return nothing
- */
-void LaneDetection::setInputImg(cv::Mat& inputImg) {
-}
-/**
- *   @brief Function to set undistorted image
- *
- *   @param undistorted image of type cv::Mat
- *   @return nothing
- */
-void LaneDetection::setUndistortedImg(cv::Mat& undistortedImg) {
-}
-/**
- *   @brief Function to set denoised image
- *
- *   @param denoised image of type cv::Mat
- *   @return nothing
- */
-void LaneDetection::setDenoisedImg(cv::Mat& denoisedImg) {
-}
-/**
- *   @brief Function to set masked image
- *
- *   @param masked image of type cv::Mat
- *   @return nothing
- */
-void LaneDetection::setMaskedImg(cv::Mat& maskedImg) {
-}
-/**
- *   @brief Function to set binary image
- *
- *   @param binary image of type cv::Mat
- *   @return nothing
- */
-void LaneDetection::setBinaryImg(cv::Mat& binaryImg) {
-}
-/**
- *   @brief Function to set right lane pixel location
- *
- *   @param set of pixels consisting of right lane, type std::vector<cv::Point_<int>>
- *   @return nothing
- */
-void LaneDetection::setRightLane(std::vector<cv::Point_<int>>& rightLane) {
-}
-/**
- *   @brief Function to set left lane pixel location
- *
- *   @param set of pixels consisting of left lane, type std::vector<cv::Point_<int>>
- *   @return nothing
- */
-void LaneDetection::setLeftLane(std::vector<cv::Point_<int>>& leftLane) {
-}
-/**
- *   @brief Function to set central line pixel location
- *
- *   @param set of pixels consisting of central line, type std::vector<cv::Point_<int>>
- *   @return nothing
- */
-void LaneDetection::setCentralLine(std::vector<cv::Point_<int>>& centralLine) {
-}
-/**
- *   @brief Function to set HSL color space threshold
- *
- *   @param threshold values for hue,saturation and luminousness, type cv::Vec<double, 3>
- *   @return nothing
- */
-void LaneDetection::setThreshHSL(cv::Vec<double, 3>& threshHSL) {
-}
-/**
- *   @brief Function to set RGB color space threshold
- *
- *   @param threshold values for red,green and blue, type cv::Vec<double, 3>
- *   @return nothing
- */
-void LaneDetection::setThreshBGR(cv::Vec<double, 3>& threshBGR) {
-}
-/**
- *   @brief Function to set lane pixel histogram
- *
- *   @param histogram representing the lane pixels along y-direction
- *   @return nothing
- */
-void LaneDetection::setHist(cv::Mat& hist) {
-}
-/**
- *   @brief Function to get size of the input image
- *
- *   @param nothing
- *   @return input image size of type cv::Size_<int>
- */
-cv::Size_<int> LaneDetection::getImgSize(void) {
-  return cv::Size_<int>;
-}
-/**
- *   @brief Function to get input image
- *
- *   @param nothing
- *   @return input image of type cv::Mat
- */
-cv::Mat LaneDetection::getInputImg(void) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to get undistorted image
- *
- *   @param nothing
- *   @return undistorted image of type cv::Mat
- */
-cv::Mat LaneDetection::getUndistortedImg(void) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to get denoised image
- *
- *   @param nothing
- *   @return denoised image of type cv::Mat
- */
-cv::Mat LaneDetection::getDenoisedImg(void) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to get masked image
- *
- *   @param nothing
- *   @return masked image of type cv::Mat
- */
-cv::Mat LaneDetection::getMaskedImg(void) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to get binary image
- *
- *   @param nothing
- *   @return binary image of type cv::Mat
- */
-cv::Mat LaneDetection::getBinaryImg(void) {
-  return cv::Mat;
-}
-/**
- *   @brief Function to get right lane pixel location
- *
- *   @param nothing
- *   @return set of pixels consisting of right lane, type std::vector<cv::Point_<int>>
- */
-std::vector<cv::Point_<int>> LaneDetection::getRightLane(void) {
-  return std::vector<cv::Point_<int>>;
-}
-/**
- *   @brief Function to get left lane pixel location
- *
- *   @param nothing
- *   @return set of pixels consisting of left lane, type std::vector<cv::Point_<int>>
- */
-std::vector<cv::Point_<int>> LaneDetection::getLeftLane(void) {
-  return std::vector<cv::Point_<int>>;
-}
-/**
- *   @brief Function to get central line pixel location
- *
- *   @param nothing
- *   @return set of pixels consisting of central line, type std::vector<cv::Point_<int>>
- */
-std::vector<cv::Point_<int>> LaneDetection::getCentralLine(void) {
-  return std::vector<cv::Point_<int>>;
-}
-/**
- *   @brief Function to get HSL color space threshold
- *
- *   @param nothing
- *   @return threshold values for hue,saturation and luminousness, type cv::Vec<double, 3>
- */
-cv::Vec<double, 3> LaneDetection::getThreshHSL(void) {
-  return cv::Vec<double, 3>;
-}
-/**
- *   @brief Function to get RGB color space threshold
- *
- *   @param nothing
- *   @return threshold values for red,green and blue, type cv::Vec<double, 3>
- */
-cv::Vec<double, 3> LaneDetection::getThreshBGR(void) {
-  return cv::Vec<double, 3>;
-}
-/**
- *   @brief Function to get lane pixel histogram
- *
- *   @param nothing
- *   @return histogram representing the lane pixels along y-direction
- */
-cv::Mat LaneDetection::getHist(void) {
-  return cv::Mat;
 }
--- include/LaneDetection.hpp
+++ include/LaneDetection.hpp
@@ -1,7 +1,33 @@
+/************************************************************************
+ MIT License
+
+ Copyright (c) 2018 Rohith Jayarajan, Akash Guha
+
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ *************************************************************************/
 /**
- *  Copyright 2018 Akash Guha
+ *  Copyright 2018 Akash Guha, rohithjayarajan
  *  @file    LaneDetection.hpp
- *  @author  Akash Guha
+ *  @author  Akash Guha, rohithjayarajan
+ *  @date 10/13/2018
+ *  @version 1.1
  *
  *  @brief Lane Detection Class Header
  *
@@ -20,22 +46,11 @@
 #include <string>
 #include <vector>
 #include "opencv2/core/core.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 class LaneDetection {
  private:
-  cv::Size_<int> imgSize;  // size of the input image
-  cv::Mat inputImg;  // variable to hold the input image
-  cv::Mat undistortedImg;  // variable to hold undistorted image
-  cv::Mat denoisedImg;  // variable to hold denoised image
-  cv::Mat maskedImg;  // variable to hold masked image
-  cv::Mat binaryImg;  // variable to hold binary image
-  std::vector<cv::Point_<int>> rightLane;  // right lane pixels
-  std::vector<cv::Point_<int>> leftLane;  // left lane pixels
-  std::vector<cv::Point_<int>> centralLine;  // central line pixels
-  cv::Vec<double, 3> threshHSL;  // HSL color space threshold values
-  cv::Vec<double, 3> threshBGR;  // RGB color space threshold values
-  cv::Mat hist;  // lane pixel histogram
-
  public:
   /**
    *   @brief Default constructor for LaneDetection
@@ -50,75 +65,55 @@ class LaneDetection {
    *   @param nothing
    *   @return nothing
    */
-  virtual ~LaneDetection();
-  /**
-   *   @brief Function to undistort the input image
-   *
-   *   @param input image of type cv::Mat
-   *   @return undistorted input image of type cv::Mat
-   */
-  cv::Mat unDistort(cv::Mat inputImg);
-  /**
-   *   @brief Function to denoise undistorted input image
-   *
-   *   @param undistorted image of type cv::Mat
-   *   @return denoised image of type cv::Mat
-   */
-  cv::Mat deNoise(cv::Mat undistortedImg);
-  /**
-   *   @brief Function to denoise image
-   *
-   *   @param denoised image of type cv::Mat
-   *   @return masked image of type cv::Mat
-   */
-  cv::Mat ROI(cv::Mat denoisedImg);
-  /**
-   *   @brief Function to get a binary image based on the color thresholding
-   *
-   *   @param masked image of type cv::Mat
-   *   @param thresholding color space of type string
-   *   @return binary image of type cv::Mat
-   */
-  cv::Mat colorThreshold(cv::Mat maskedImg, std::string colorSpace);
+  ~LaneDetection();
   /**
    *   @brief Function to generate lane pixel histogram
    *
    *   @param projective transform of binary image of type cv::Mat
-   *   @return histogram containing lane pixels of type cv::Mat
+   *   @param histogram containing lane pixels of type cv::Mat
+   *   @return nothing
    */
-  cv::Mat generateHist(cv::Mat birdView);
+  void generateHist(cv::Mat& src, cv::Mat& hist);
   /**
    *   @brief Function to extract left or right lane
    *
    *   @param histogram containing lane pixels of type cv::Mat
+   *   @param pixel locations of left or right lane, type std::vector<cv::Point_<int>>
    *   @param lane to be extracted - left or right of type string
-   *   @return pixel locations of left or right lane, type std::vector<cv::Point_<int>>
+   *   @return nothing
    */
-  std::vector<cv::Point_<int>> extractLane(cv::Mat hist, std::string lane);
+  void extractLane(cv::Mat& hist, std::vector<cv::Point_<int>>& dstLane,
+                   std::string& laneType);
   /**
    *   @brief Function to fit a polynomial on the received lane pixel data
    *
    *   @param left or right lane pixel locations, type std::vector<cv::Point_<int>>
-   *   @return pixel locations fitted to a particular polynomial, type std::vector<cv::Point_<int>>
+   *   @param pixel locations fitted to a particular polynomial, type std::vector<cv::Point_<int>>
+   *   @return nothing
    */
-  std::vector<cv::Point_<int>> fitPoly(std::vector<cv::Point_<int>> laneLR);
+  void fitPoly(std::vector<cv::Point_<int>> laneLR,
+               std::vector<cv::Point_<int>> dstPixelPoly);
   /**
    *   @brief Function to extract central line
    *
    *   @param left lane pixel locations, type std::vector<cv::Point_<int>>
    *   @param right lane pixel locations, type std::vector<cv::Point_<int>>
-   *   @return central line pixel locations, type std::vector<cv::Point_<int>>
+   *   @param central line pixel locations, type std::vector<cv::Point_<int>>
+   *   @return nothing
    */
-  std::vector<cv::Point_<int>> extractCentralLine(
+  void extractCentralLine(
       std::vector<cv::Point_<int>> leftLane,
-      std::vector<cv::Point_<int>> rightLane);
+                          std::vector<cv::Point_<int>> rightLane,
+                          std::vector<cv::Point_<int>> centralLine);
   /**
    *   @brief Function to compute slope of the central line
    *
    *   @param central line pixel location, type std::vector<cv::Point_<int>>
-   *   @return string, indicating left or right turn
+   *   @param string, indicating left or right turn
+   *   @return nothing
    */
-  std::string computeGradient(std::vector<cv::Point_<int>> centralLine);
+  void computeGradient(std::vector<cv::Point_<int>> centralLine,
+                       std::string steerDirection);
   /**
    *   @brief Function to compute radius of curvature of the lane
    *
@@ -126,177 +121,6 @@ class LaneDetection {
    *   @return radius of curvature, type double
    */
   double computeRadiusOfCurve(std::vector<cv::Point_<int>> centralLine);
-  /**
-   *   @brief Function to set size of the input image
-   *
-   *   @param input image size of type cv::Size_<int>
-   *   @return nothing
-   */
-  void setImgSize(cv::Size_<int>& imgSize);
-  /**
-   *   @brief Function to set input image
-   *
-   *   @param input image of type cv::Mat
-   *   @return nothing
-   */
-  void setInputImg(cv::Mat& inputImg);
-  /**
-   *   @brief Function to set undistorted image
-   *
-   *   @param undistorted image of type cv::Mat
-   *   @return nothing
-   */
-  void setUndistortedImg(cv::Mat& undistortedImg);
-  /**
-   *   @brief Function to set denoised image
-   *
-   *   @param denoised image of type cv::Mat
-   *   @return nothing
-   */
-  void setDenoisedImg(cv::Mat& denoisedImg);
-  /**
-   *   @brief Function to set masked image
-   *
-   *   @param masked image of type cv::Mat
-   *   @return nothing
-   */
-  void setMaskedImg(cv::Mat& maskedImg);
-  /**
-   *   @brief Function to set binary image
-   *
-   *   @param binary image of type cv::Mat
-   *   @return nothing
-   */
-  void setBinaryImg(cv::Mat& binaryImg);
-  /**
-   *   @brief Function to set right lane pixel location
-   *
-   *   @param set of pixels consisting of right lane, type std::vector<cv::Point_<int>>
-   *   @return nothing
-   */
-  void setRightLane(std::vector<cv::Point_<int>>& rightLane);
-  /**
-   *   @brief Function to set left lane pixel location
-   *
-   *   @param set of pixels consisting of left lane, type std::vector<cv::Point_<int>>
-   *   @return nothing
-   */
-  void setLeftLane(std::vector<cv::Point_<int>>& leftLane);
-  /**
-   *   @brief Function to set central line pixel location
-   *
-   *   @param set of pixels consisting of central line, type std::vector<cv::Point_<int>>
-   *   @return nothing
-   */
-  void setCentralLine(std::vector<cv::Point_<int>>& centralLine);
-  /**
-   *   @brief Function to set HSL color space threshold
-   *
-   *   @param threshold values for hue,saturation and luminousness, type cv::Vec<double, 3>
-   *   @return nothing
-   */
-  void setThreshHSL(cv::Vec<double, 3>& threshHSL);
-  /**
-   *   @brief Function to set RGB color space threshold
-   *
-   *   @param threshold values for red,green and blue, type cv::Vec<double, 3>
-   *   @return nothing
-   */
-  void setThreshBGR(cv::Vec<double, 3>& threshBGR);
-  /**
-   *   @brief Function to set lane pixel histogram
-   *
-   *   @param histogram representing the lane pixels along y-direction
-   *   @return nothing
-   */
-  void setHist(cv::Mat& hist);
-  /**
-   *   @brief Function to get size of the input image
-   *
-   *   @param nothing
-   *   @return input image size of type cv::Size_<int>
-   */
-  cv::Size_<int> getImgSize(void);
-  /**
-   *   @brief Function to get input image
-   *
-   *   @param nothing
-   *   @return input image of type cv::Mat
-   */
-  cv::Mat getInputImg(void);
-  /**
-   *   @brief Function to get undistorted image
-   *
-   *   @param nothing
-   *   @return undistorted image of type cv::Mat
-   */
-  cv::Mat getUndistortedImg(void);
-  /**
-   *   @brief Function to get denoised image
-   *
-   *   @param nothing
-   *   @return denoised image of type cv::Mat
-   */
-  cv::Mat getDenoisedImg(void);
-  /**
-   *   @brief Function to get masked image
-   *
-   *   @param nothing
-   *   @return masked image of type cv::Mat
-   */
-  cv::Mat getMaskedImg(void);
-  /**
-   *   @brief Function to get binary image
-   *
-   *   @param nothing
-   *   @return binary image of type cv::Mat
-   */
-  cv::Mat getBinaryImg(void);
-  /**
-   *   @brief Function to get right lane pixel location
-   *
-   *   @param nothing
-   *   @return set of pixels consisting of right lane, type std::vector<cv::Point_<int>>
-   */
-  std::vector<cv::Point_<int>> getRightLane(void);
-  /**
-   *   @brief Function to get left lane pixel location
-   *
-   *   @param nothing
-   *   @return set of pixels consisting of left lane, type std::vector<cv::Point_<int>>
-   */
-  std::vector<cv::Point_<int>> getLeftLane(void);
-  /**
-   *   @brief Function to get central line pixel location
-   *
-   *   @param nothing
-   *   @return set of pixels consisting of central line, type std::vector<cv::Point_<int>>
-   */
-  std::vector<cv::Point_<int>> getCentralLine(void);
-  /**
-   *   @brief Function to get HSL color space threshold
-   *
-   *   @param nothing
-   *   @return threshold values for hue,saturation and luminousness, type cv::Vec<double, 3>
-   */
-  cv::Vec<double, 3> getThreshHSL(void);
-  /**
-   *   @brief Function to get RGB color space threshold
-   *
-   *   @param nothing
-   *   @return threshold values for red,green and blue, type cv::Vec<double, 3>
-   */
-  cv::Vec<double, 3> getThreshBGR(void);
-  /**
-   *   @brief Function to get lane pixel histogram
-   *
-   *   @param nothing
-   *   @return histogram representing the lane pixels along y-direction
-   */
-  cv::Mat getHist(void);
 };
-
-
-
 
 #endif  // INCLUDE_LANEDETECTION_HPP_
